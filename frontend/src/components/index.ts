6fa9be619aa4a087ed9aad8561f4c64b8d584d93
jbuendia1y <jgamer669@gmail.com>
2023-03-05 11:50:58 -0500

    ProtectedRoute componente

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as LinkCard } from "./LinkCard";
 export { default as LinkView } from "./LinkView";
 export { default as Sidenav } from "./Sidenav";
+
+export { default as ProtectedRoute } from "./ProtectedRoute";
