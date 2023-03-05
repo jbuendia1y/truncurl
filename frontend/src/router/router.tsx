906519f5e2feaed9385ed63b70e68800c40a1c68
jbuendia1y <jgamer669@gmail.com>
2023-03-05 11:51:21 -0500

    + Router

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,87 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ProtectedRoute } from "../components";
+import {
+  Configuration,
+  EnterCode,
+  Home,
+  Links,
+  Login,
+  NewPassword,
+  RecoverPassword,
+  Register,
+  Tags,
+} from "../pages";
+import { Profile, Webhooks } from "../pages/config/views";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/auth",
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "recover-password",
+        children: [
+          {
+            path: "",
+            element: <RecoverPassword />,
+          },
+          {
+            path: "code",
+            element: <EnterCode />,
+          },
+          {
+            path: "reset",
+            element: <NewPassword />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/links",
+    element: (
+      <ProtectedRoute>
+        <Links />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/tags",
+    element: (
+      <ProtectedRoute>
+        <Tags />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/settings",
+    element: (
+      <ProtectedRoute>
+        <Configuration />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "webhooks",
+        element: <Webhooks />,
+      },
+    ],
+  },
+]);
+
+export default router;
