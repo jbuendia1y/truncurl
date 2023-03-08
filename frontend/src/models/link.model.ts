ce52905ab1035c872c68d98b1295a06c14355926
jbuendia1y <jgamer669@gmail.com>
2023-03-08 11:22:58 -0500

    Link service and model

--- frontend/src/models/link.model.ts
+++ frontend/src/models/link.model.ts
@@ -2,14 +2,30 @@ export interface IEndpointLink {
   name?: string;
   url: string;
   hash: string;
-  user_id?: string;
+  user_id: string;
   created_at: string;
 }
 
+export interface ICreateLink {
+  url: string;
+  name?: string;
+}
+
 export interface ILink {
+  id: string;
   name?: string;
   url: string;
   hash: string;
-  userId?: string;
+  userId: string;
   createdAt: Date;
+}
+
+export interface LinkFilter {
+  name?: string;
+  url?: string;
+  hash?: string;
+  createdAt?: {
+    $gt: Date;
+    $lt: Date;
+  };
 }
