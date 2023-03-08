5103a0f9c365331ba36faaf180485f9daa9321ad
jbuendia1y <jgamer669@gmail.com>
2023-03-08 11:28:57 -0500

    useMediaQuery hook en el sidenav.context

--- frontend/src/contexts/sidenav.context.ts
+++ frontend/src/contexts/sidenav.context.ts
@@ -1,5 +1,9 @@
 import { createContext } from "react";
 
-const SidenavCtx = createContext({ open: false, toggle: () => {} });
+const SidenavCtx = createContext({
+  open: false,
+  toggle: () => {},
+  isLargerThan900: false,
+});
 
 export default SidenavCtx;
--- frontend/src/providers/sidenav.provider.tsx
+++ frontend/src/providers/sidenav.provider.tsx
@@ -1,8 +1,10 @@
+import { useMediaQuery } from "@chakra-ui/react";
 import { useState } from "react";
 import { SidenavCtx } from "../contexts";
 
 const SidenavProvider = (props: { children: any }) => {
   const { children } = props;
+  const [isLargerThan900] = useMediaQuery("(min-width: 48em)");
   const [open, setOpen] = useState(false);
 
   const toggle = () => {
@@ -10,7 +12,7 @@ const SidenavProvider = (props: { children: any }) => {
   };
 
   return (
-    <SidenavCtx.Provider value={{ open, toggle }}>
+    <SidenavCtx.Provider value={{ open, toggle, isLargerThan900 }}>
       {children}
     </SidenavCtx.Provider>
   );
