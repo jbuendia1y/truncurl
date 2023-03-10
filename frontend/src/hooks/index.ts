db0670fd4483edc00d29fa592b23f946944572d3
jbuendia1y <jgamer669@gmail.com>
2023-03-10 11:51:55 -0500

    useTags hook

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { default as useSidenav } from './useSidenav';
 export { default as useAuth } from './useAuth';
 export { default as useLinks } from './useLinks';
+export { default as useTags } from './useTags';
+export { default as useDisplay } from './useDisplay';
